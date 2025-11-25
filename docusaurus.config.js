7bb85ae110b1b12ba50ca1b1ad73bbaf6b5ea0e1
Gonçalo Augusto <goncaloaugusto2006@hotmail.com>
2025-11-25 15:54:15 +0000

    Alterrei a tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Turkey',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Barbie girl :)',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
