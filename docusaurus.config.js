a4690ad3958eda97553afb833b4ce7b94a4604a2
Gonçalo Augusto <goncaloaugusto2006@hotmail.com>
2025-11-25 16:04:05 +0000

    um erro de proposito

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Turkey',
-  tagline: 'Barbie girl :)',
+  tagline: 'Barbie girl :)  a',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
